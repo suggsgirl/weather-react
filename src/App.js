6dad55fe6127ac5bd9132b0ca1da447fe767e449
suggsgirl <suggsgirl@hotmail.com>
2021-04-29 15:19:44 +0100

    Unit conversion

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 export default function App() {
   return (
     <div className="App">
-      <MainBody defaultCity="Paris"/>
+      <MainBody defaultCity="London"/>
       <Footer />
     </div>
   );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature"; 
 
 export default function WeatherInfo(props) {
   return (
@@ -26,12 +27,10 @@ export default function WeatherInfo(props) {
               <div className="float-left">
                 <WeatherIcon code={props.data.icon} />
               </div>
-              <div className="float-left">
-                <span class="temperature" id="showTemperature">
-                  {" "}
-                  {Math.round(props.data.temperature)}{" "}
-                </span>
-                <span class="units">°C |°F</span>
+             
+              <div className="float-left"> 
+              <WeatherTemperature celsius={props.data.temperature}/> 
+                
               </div>
             </div>
           </div>
