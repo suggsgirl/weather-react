e4e895307181474ecb211314c6e139e89417a547
suggsgirl <suggsgirl@hotmail.com>
2021-04-29 15:25:49 +0100

    api URL updated

--- src/MainBody.js
+++ src/MainBody.js
@@ -21,7 +21,7 @@ export default function MainBody(props) {
 
   function search() {
     const apiKey = "db6cd03093ca5ac508123ad570a470a9";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
