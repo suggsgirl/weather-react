50035cf493143a08f3ad7608d9f64fbad8c77d14
suggsgirl <suggsgirl@hotmail.com>
2021-04-29 13:36:17 +0100

    New icons added

--- src/MainBody.js
+++ src/MainBody.js
@@ -12,7 +12,7 @@ export default function MainBody(props) {
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,49 +1,49 @@
 import React from "react";
-import FormattedDate from "./FormattedDate"; 
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
-    return (
-      <div className="card w-100 today">
-        <div className="card-body">
-          <h1>
-            {" "}
-            <i className="fas fa-map-marker-alt"></i>{" "}
-            <span>{props.data.city}</span>{" "}
-          </h1>
+  return (
+    <div className="card w-100 today">
+      <div className="card-body">
+        <h1>
+          {" "}
+          <i className="fas fa-map-marker-alt"></i>{" "}
+          <span>{props.data.city}</span>{" "}
+        </h1>
 
-          <h3>
-            <i className="far fa-clock"></i>{" "}
-            <span>
-              <FormattedDate date={props.data.date} />
-            </span>
-          </h3>
-          <div className="row">
-            <div className="col-7">
-              <div className="clearfix weather-temperature">
-                <img
-                  src={props.data.iconUrl}
-                  alt="Cloudy"
-                  id="icon"
-                  class="float-left"
-                />
-                <div className="float-left">
-                  <span class="temperature" id="showTemperature">
-                    {" "}
-                    {Math.round(props.data.temperature)}{" "}
-                  </span>
-                  <span class="units">°C |°F</span>
-                </div>
+        <h3>
+          <i className="far fa-clock"></i>{" "}
+          <span>
+            <FormattedDate
+              date={props.data.date}
+            />
+          </span>
+        </h3>
+        <div className="row">
+          <div className="col-7">
+            <div className="clearfix weather-temperature">
+              <div className="float-left">
+                <WeatherIcon code={props.data.icon} />
+              </div>
+              <div className="float-left">
+                <span class="temperature" id="showTemperature">
+                  {" "}
+                  {Math.round(props.data.temperature)}{" "}
+                </span>
+                <span class="units">°C |°F</span>
               </div>
             </div>
-            <div class="col-5">
-              <ul>
-                <li className="text-capitalize">{props.data.description}</li>
-                <li>Humidity: {props.data.humidity}% </li>
-                <li>Wind: {Math.round(props.data.wind)}km/h</li>
-              </ul>
-            </div>
+          </div>
+          <div class="col-5">
+            <ul>
+              <li className="text-capitalize">{props.data.description}</li>
+              <li>Humidity: {props.data.humidity}% </li>
+              <li>Wind: {Math.round(props.data.wind)}km/h</li>
+            </ul>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
